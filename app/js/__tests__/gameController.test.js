3604ec1529966e254e33726c4813702082297d9f
DND-dev <dev.work@dnd-projects.ru>
2023-03-12 00:06:07 +0300

    fixed tests

--- app/js/__tests__/gameController.test.js
+++ app/js/__tests__/gameController.test.js
@@ -11,7 +11,7 @@ describe("Bases Logic", () => {
 	test("test message of unit options", () => {
 		const unit = new Bowman(1)
 		const result = generateMessage(unit.level, unit.attack, unit.defence, unit.health)
-		expect(result).toBe("\u{1F396} 1 \u{1F5E1} 25 \u{1F6E1} 25 \u{2764} 50")
+		expect(result).toBe("\u{1F396} 1 \u{1F5E1} 150 \u{1F6E1} 25 \u{2764} 50")
 	})
 })
 
